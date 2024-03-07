624ad6823b2e6058bbc29fd4f4967abe73b37e55
Firadi <122645437+Firadi@users.noreply.github.com>
2024-03-07 01:57:03 +0100

    game interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,19 @@
 /* eslint-disable max-len */
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { DialogModule } from '@angular/cdk/dialog';
-
 import { AppComponent } from './app.component';
 import { BoardComponent } from './components/board/board.component';
 import { SquareComponent } from './components/square/square.component';
 import { SquareColorPipe } from './pipes/square-color.pipe';
-// eslint-disable-next-line max-len
 import { PromoteDialogComponent } from './components/promote-dialog/promote-dialog.component';
-
 import { AppRoutingModule } from './app-routing.module';
 import { IndexComponent } from './components/platform/dashboard/index/index.component';
 import { SidebarComponent } from './components/platform/dashboard/sidebar/sidebar.component';
 import { SigninComponent } from './components/platform/signin/signin.component';
 import { GameOnlineComponent } from './components/platform/game-interface/game-online/game-online.component';
 import { MainComponent } from './components/platform/dashboard/main/main.component';
-import { GameOnlineBoardComponent } from './platform/game-interface/game-online/game-online-board/game-online-board.component';
-
+import { GameOnlineBoardComponent } from './components/platform/game-interface/game-online/game-online-board/game-online-board.component';
 
 @NgModule({
   imports: [
--- src/app/components/platform/game-interface/game-online/game-online-board/game-online-board.component.ts
+++ src/app/components/platform/game-interface/game-online/game-online-board/game-online-board.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'jv-game-online-board',
   templateUrl: './game-online-board.component.html',
-  styleUrls: ['./game-online-board.component.scss']
+  styleUrls: ['./game-online-board.component.scss'],
 })
 export class GameOnlineBoardComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
