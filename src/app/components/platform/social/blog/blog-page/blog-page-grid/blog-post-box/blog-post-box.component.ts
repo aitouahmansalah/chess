a71685dfbb21c6617f0b7fba6d2a5c44c9bbfdc4
Firadi <122645437+Firadi@users.noreply.github.com>
2024-03-18 04:21:19 +0000

    fixing zoom post

--- src/app/components/platform/social/blog/blog-page/blog-page-grid/blog-post-box/blog-post-box.component.ts
+++ src/app/components/platform/social/blog/blog-page/blog-page-grid/blog-post-box/blog-post-box.component.ts
@@ -8,33 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog-post-box.component.scss'],
 })
 export class BlogPostBoxComponent {
-  postFontSize: number;
+  // postFontSize: number;
 
   constructor() {
-    const postElement = document.querySelector(".box .post");
-    if (postElement) {
-      this.postFontSize = parseFloat(window.getComputedStyle(postElement).fontSize);
-    } else {
-      throw new Error("No element with class 'post' found.");
-    }
+  //   const postElement = document.querySelector(".box .post");
+  //   if (postElement) {
+  //     this.postFontSize = parseFloat(window.getComputedStyle(postElement).fontSize);
+  //   } else {
+  //     throw new Error("No element with class 'post' found.");
+  //   }
   }
 
-  zoomIn():void{
-    const postElement = document.querySelector(".post") as HTMLElement;
-    if (postElement) {
-      if (this.postFontSize < 31) {
-        this.postFontSize += 2; 
-        postElement.style.fontSize = this.postFontSize + "px";
-      }
-    }
-  }
-  zoomOut():void{
-    const postElement = document.querySelector(".post") as HTMLElement;
-    if (postElement) {
-      if (this.postFontSize > 15) {
-        this.postFontSize -= 2; 
-        postElement.style.fontSize = this.postFontSize + "px";
-      }
-    }
-  }
+  // zoomIn():void{
+  //   const postElement = document.querySelector(".post") as HTMLElement;
+  //   if (postElement) {
+  //     if (this.postFontSize < 31) {
+  //       this.postFontSize += 2; 
+  //       postElement.style.fontSize = this.postFontSize + "px";
+  //     }
+  //   }
+  // }
+  // zoomOut():void{
+  //   const postElement = document.querySelector(".post") as HTMLElement;
+  //   if (postElement) {
+  //     if (this.postFontSize > 15) {
+  //       this.postFontSize -= 2; 
+  //       postElement.style.fontSize = this.postFontSize + "px";
+  //     }
+  //   }
+  // }
 }
--- src/app/directives/zoom.directive.ts
+++ src/app/directives/zoom.directive.ts
@@ -1,35 +1,35 @@
+/* eslint-disable max-len */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
-  selector: '[jvZoom]'
+  selector: '[jvZoom]',
 })
 export class ZoomDirective {
+  @Input() zoomScale: number = 15;
 
-  @Input() zoomScale: number = 1.2;
-
-  @Input() zoomMax: number = 30;
+  @Input() zoomMax: number = 31;
 
   @Input() zoomMin: number = 15;
 
   @Input('appZoom') zoomDirection: 'in' | 'out' = 'in'; 
 
-  @Input() zoomTarget!:ElementRef;
-
-
-
+  @Input() zoomTarget!: HTMLElement;
 
   constructor(private elementRef: ElementRef) { }
 
   @HostListener('click') onClick() {
+    this.zoomScale = parseInt(this.zoomTarget.style.fontSize) ? parseInt(this.zoomTarget.style.fontSize) : 15
     if(this.zoomDirection == 'in' && this.zoomMax > this.zoomScale)
-    this.zoomScale += 4;
+    this.zoomScale += 2;
     if(this.zoomDirection == 'out' && this.zoomMin < this.zoomScale)
-    this.zoomScale -= 4;
+    this.zoomScale -= 2;
+
     this.zoom(this.zoomScale);
   }
 
   private zoom(scale: number) {
-    this.zoomTarget.nativeElement.style.transform = `scale(${scale})`;
+    this.zoomTarget.style.fontSize = `${scale}px`;
+    console.log(scale)
   }
-
 }
